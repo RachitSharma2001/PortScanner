4d072f056bc6330a331aa325ad4c7ea1c3140c00
RachitSharma2001 <rachitsharma613@gmail.com>
2022-04-09 09:43:55 -0700

    Added concurrent scan; much faster

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 )
 
-func ScanTcp(startPort, endPort int) {
+func ScanTCP(startPort, endPort int) {
 	dialLimit := time.Microsecond
 	for port := startPort; port <= endPort; port++ {
 		_, err := net.DialTimeout("tcp", fmt.Sprintf(":%d", port), dialLimit)
 		if err == nil {
-			fmt.Printf("Port %d is open\n", port)
+			fmt.Printf("TCP Port %d is open\n", port)
 		}
 	}
 }
 
+func ScanTCPConcurrently(startPort, endPort int) {
+	var wg sync.WaitGroup
+	for port := startPort; port <= endPort; port++ {
+		wg.Add(1)
+		go func(port int) {
+			portStr := getPortString(port)
+			_, err := net.Dial("tcp", portStr)
+			if err == nil {
+				fmt.Printf("TCP Port %d is open\n", port)
+			}
+			wg.Done()
+		}(port)
+	}
+	wg.Wait()
+}
+
+func getPortString(port int) string {
+	return fmt.Sprintf(":%d", port)
+}
+
 func main() {
-	ScanTcp(1, 1024)
+	fmt.Println("Starting Synchronous Scan")
+	ScanTCP(1, 1024)
+	fmt.Println("Starting Concurrent Scan")
+	ScanTCPConcurrently(1, 1024)
 }
