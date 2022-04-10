25d65d1398daf33d5e28d7e38657157a5bfa35dc
RachitSharma2001 <rachitsharma613@gmail.com>
2022-04-10 11:25:05 -0700

    Added BDD test for TCP scanning

--- tests/port_test.go
+++ tests/port_test.go
@@ -1,8 +1,12 @@
 package main_test
 
 import (
+	"net"
+	"strconv"
+	"sync"
 	"testing"
 
+	port "fake.com/PortScanner/port"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -13,14 +17,32 @@ func TestPortScanner(t *testing.T) {
 }
 
 var _ = Describe("Scanning TCP Ports", func() {
+	portToListen := 3000
+	host := "localhost"
+	portRep := host + ":" + strconv.Itoa(portToListen)
+	var listener net.Listener
 	Context("if we scan an open TCP port", func() {
-		It("the scan should be successful", func() {
-
+		listener, _ = net.Listen("tcp", portRep)
+		var scanRes port.ScanResults
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go port.ScanIndividualTcpPort(portToListen, host, &scanRes, &wg)
+		wg.Wait()
+		openPorts := scanRes.OpenPorts
+		It("the scan should detect the open port", func() {
+			Expect(openPorts).To(ContainElement(portToListen))
 		})
 	})
+	listener.Close()
 	Context("if we scan a closed TCP port", func() {
-		It("the scan should be unsuccessful", func() {
-
+		var scanRes port.ScanResults
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go port.ScanIndividualTcpPort(portToListen, host, &scanRes, &wg)
+		wg.Wait()
+		openPorts := scanRes.OpenPorts
+		It("the scan should detect no open ports", func() {
+			Expect(openPorts).To(BeEmpty())
 		})
 	})
 })
